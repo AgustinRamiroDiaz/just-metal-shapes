32a7faf13b25b656f8c8b5ffb9a23e780a9d4084
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2026-04-04 09:35:45 -0300

    refactor

--- scripts/components/spawner_component.gd
+++ scripts/components/spawner_component.gd
@@ -8,27 +8,22 @@ signal spawned(node: Node)
 @export var spawn_area_width: float = 800.0
 @export var spawn_y_offset: float = -50.0
 
-var spawn_timer: float = 0.0
+@onready var timer: Timer = $Timer
 
 
 func _ready() -> void:
 	if enemy_scene == null:
 		enemy_scene = load("res://scenes/enemy.tscn")
-	spawn_timer = randf_range(0.0, spawn_interval)
-
-
-func _process(delta: float) -> void:
-	spawn_timer += delta
-	if spawn_timer >= spawn_interval:
-		spawn_timer = 0.0
-		_spawn()
+	timer.wait_time = spawn_interval
+	timer.start(randf_range(0.0, spawn_interval))
+	timer.timeout.connect(_spawn)
 
 
 func _spawn() -> void:
 	var scene_to_spawn := enemy_scene
 	if enemy_scene == null:
 		scene_to_spawn = load("res://scenes/enemy.tscn")
-	
+
 	var enemy: StaticBody2D = scene_to_spawn.instantiate()
 	var spawn_x := randf_range(0.0, spawn_area_width)
 	enemy.global_position = Vector2(spawn_x, spawn_y_offset)
